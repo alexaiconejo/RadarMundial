9045fb9f40d37d60483702e38f458e3b2bd48df0
alexaiconejo <fondo.gif>
2023-10-04 14:49:19 -0300

    first commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 const sheetKey = import.meta.env.VITE_SHEET_KEY;
 
 const constants = {
-  sheetUrl: `https://sheets.googleapis.com/v4/spreadsheets/1h1yoCZfbFKLWqvxsqbCwGkJtKIbak6vEXqJnK_7vYDI/values/1!A:H?key=${sheetKey}`,
+  sheetUrl: `https://sheets.googleapis.com/v4/spreadsheets/12OZcqN7sl6pEAQhQqGcB5HxbLnFEfzyvf1iNsdJGrE0/values/1!A:H?key=${sheetKey}`,
 };
 
 export default constants;
