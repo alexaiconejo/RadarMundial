8c4e1c7b1d9c1a62b5f5f13bae2ebca06c71b665
alexaiconejo <fondo.gif>
2023-10-04 14:54:22 -0300

    Updates

--- data/mapsData/index.js
+++ data/mapsData/index.js
@@ -1,7 +1,7 @@
 import provincias from "./provincias.json";
 import departamentos from "./departamentos-argentina.json";
-import departamentosBsAs from "./departamentos-buenos_aires.json";
-import rutas from "./rutas.json";
+import departamentosBsAs from "./departamentos-argentina.json";
+import rutas from './rutas.json'
 
 
-export { provincias, departamentos, departamentosBsAs, rutas};
+export { provincias, departamentos, departamentosBsAs, rutas, };
--- src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-const sheetKey = import.meta.env.VITE_SHEET_KEY;
-
-const constants = {
-  sheetUrl: `https://sheets.googleapis.com/v4/spreadsheets/12OZcqN7sl6pEAQhQqGcB5HxbLnFEfzyvf1iNsdJGrE0/values/1!A:H?key=${sheetKey}`,
-};
-
-export default constants;
